305e17cb8446c69691ca9b8eb34ab145f0f163f2
AnuR1234 <35266536+AnuR1234@users.noreply.github.com>
2023-08-26 09:20:33 +0200

    Update callback.py
    1.Updated the player's logic

--- My_code/callback.py
+++ My_code/callback.py
@@ -1,30 +1,38 @@
-# agent_code/callback.py
-import pygame
-
-# ... You can also import any other necessary modules or libraries here ...
-
 class Player:
-    def __init__(self, x, y, walls, crates, width, height):
+    def __init__(self, x, y, wall_positions, crate_positions, WIDTH, HEIGHT):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
-        self.walls = walls
-        self.crates = crates
+        self.prev_position = (x, y)  # Initialize the prev_position to the starting position
+        self.wall_positions = wall_positions
+        self.crate_positions = crate_positions
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+        self.recent_locations = []
+        self.visited_locations = set()
 
-    def move(self, dx, dy):
+    def get_position(self):
+        """Returns the current position (x, y) of the player."""
+        return self.x, self.y
+
+    def is_valid_move(self, dx, dy):
         new_x = self.x + dx
         new_y = self.y + dy
-        
+
         # Check boundaries
-        if new_x < 0 or new_x >= self.width or new_y < 0 or new_y >= self.height:
-            return
-        
-        if (new_x, new_y) not in self.walls and (new_x, new_y) not in self.crates:
-            self.x = new_x
-            self.y = new_y
+        if new_x < 0 or new_x >= self.WIDTH or new_y < 0 or new_y >= self.HEIGHT:
+            return False
 
-    def draw(self, screen, image):
-        screen.blit(image, (self.x, self.y))
+        # Check if the new position collides with a wall or crate
+        if (new_x, new_y) in self.wall_positions or (new_x, new_y) in self.crate_positions:
+            return False
 
+        return True
 
+    def move(self, dx, dy):
+        if self.is_valid_move(dx, dy):
+            # Update prev_position before making the move
+            self.prev_position = (self.x, self.y)
+            self.x += dx
+            self.y += dy
+
+    # Additional methods related to rendering or any other operations...
